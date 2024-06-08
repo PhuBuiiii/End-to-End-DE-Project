c777bf741b87ff512fa2045a295c6e6e8efe59de
Phu Bui <phubui2k2@gmail.com>
2024-06-08 17:43:01 +0700

    Complete Project

--- dagster_orchestration/dagster_orchestration/__init__.py
+++ dagster_orchestration/dagster_orchestration/__init__.py
@@ -1,9 +1,14 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules, AssetSelection, ScheduleDefinition, define_asset_job
+from .assets import resources
 
-from . import assets
+big_star_job = define_asset_job("big_star_job", selection=AssetSelection.all())
 
+big_star_schedule = ScheduleDefinition(
+    job=big_star_job,
+    cron_schedule="0 * * * *",  # every hour
+)
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
-    assets=all_assets,
+    assets=all_assets, resources= resources, jobs=[big_star_job], schedules=[big_star_schedule]
 )
